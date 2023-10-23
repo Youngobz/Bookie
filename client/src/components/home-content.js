d0ae8c00bce2e9469851283a274b5f6810fa6263
Youngobz <133522178+Youngobz@users.noreply.github.com>
2023-10-23 22:41:27 +1100

    Book Card UI Design Completed

--- client/src/components/home-content.js
+++ client/src/components/home-content.js
@@ -3,6 +3,36 @@ import Styled from "styled-components";
 
 const HomeWrapper = Styled.div`
   text-align: center;
+  margin: 0;
+`;
+
+const BookCardWrapper = Styled.div`
+  display: flex;
+  justify-content: start;
+  margin: 0 auto;
+  max-width: 800px;
+  flex-wrap: wrap;
+`;
+
+const BookCard = Styled.div`
+  width: 200px;
+  margin: 20px;
+  height: 200px;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.15);
+  }
+`;
+
+const BookImage = Styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+`;
+
+const BookTitle = Styled.div`
+  color: #fff;
 `;
 
 const HomeContent = (props) => {
@@ -14,19 +44,20 @@ const HomeContent = (props) => {
   return (
     <HomeWrapper>
       <h1 className="mb-4">All Books</h1>
-      <div className="row">
+      <BookCardWrapper>
         {books.map((book, index) => (
-          <div
+          <BookCard
             className="image-container d-flex justify-content-start m-3"
             key={index}
             onClick={() => {
               console.log("redirect to book");
             }}
           >
-            <img src={book.image} alt="book"></img>
-          </div>
+            <BookImage src={book.image} alt="book"></BookImage>
+            <BookTitle>{book.title}</BookTitle>
+          </BookCard>
         ))}
-      </div>
+      </BookCardWrapper>
     </HomeWrapper>
   );
 };
